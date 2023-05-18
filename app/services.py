1eb72ed2cfa4ff8bac9ad17a8b8256947703dec0
psamano <psamano@storecheck.com>
2023-05-17 18:47:44 -0600

    Para services.py en add_short_url, con esta modificación, la función add_short_url primero verifica si la URL original ya existe en la base de datos. Si es así, devuelve el objeto URL existente en lugar de crear uno nuevo. Esto evita la creación de múltiples shortcodes para la misma URL original.
    Se modifica main.py con el bloque "with app.app_context()" asegura que el contexto de la aplicación esté disponible antes de crear las tablas en la base de datos utilizando db.create_all()(evitar temas de la base de datos al iniciar la app).

--- app/services.py
+++ app/services.py
@@ -2,6 +2,10 @@ from app import db
 from app.models import URL, db
 
 def add_short_url(original_url, shortcode):
+    existing_short_url = URL.query.filter_by(original_url=original_url).first()
+    if existing_short_url:
+        return existing_short_url
+
     short_url = URL(original_url=original_url, shortcode=shortcode)
     db.session.add(short_url)
     db.session.commit()
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from app import create_app
+from app import create_app, db
 
 app = create_app()
-
-if __name__ == "__main__":
-    app.run()
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
