1a11d64802ae7577be3d405d8c0a061e9456f3d1
psamano <psamano@storecheck.com>
2023-05-17 18:33:44 -0600

    Se agrega archivo de pruebas para ejecutarse con pytest
    test_shorten_url: Verifica que al enviar una solicitud POST a /shorten con una URL válida, se recibe una respuesta con el estado 200 y un shortcode de 8 caracteres.
    test_redirect_to_original_url: Verifica que al enviar una solicitud GET a /<shortcode>, se redirige al usuario a la URL original.
    test_invalid_shortcode: Verifica que al enviar una solicitud GET a /<shortcode> con un shortcode no válido o inexistente, se recibe una respuesta con el estado 404.

---
+++ app/test_views.py
@@ -0,0 +1,46 @@
+import json
+import pytest
+from app.services import add_short_url
+from app import create_app, db
+from app.models import URL
+from app.utils import generate_shortcode
+
+app = create_app()
+
+@pytest.fixture
+def test_client():
+    app.config['TESTING'] = True
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+    with app.test_client() as test_client:
+        with app.app_context():
+            db.create_all()
+        yield test_client
+        with app.app_context():
+            db.drop_all()
+
+def test_shorten_url(test_client):
+    original_url = "https://www.example.com"
+    response = test_client.post('/shorten', data=json.dumps({"url": original_url}), content_type='application/json')
+    
+    assert response.status_code == 200
+    shortcode = json.loads(response.data)['shortcode']
+    assert len(shortcode) == 6
+
+def test_redirect_to_original_url(test_client):
+    original_url = "https://www.example.com"
+    shortcode = generate_shortcode()
+    
+    with app.app_context():
+        short_url = URL(original_url=original_url, shortcode=shortcode)
+        db.session.add(short_url)
+        db.session.commit()
+
+    response = test_client.get(f'/{shortcode}')
+    assert response.status_code == 302
+    assert response.location == original_url
+
+
+
+def test_invalid_shortcode(test_client):
+    response = test_client.get('/invalidshortcode')
+    assert response.status_code == 404
